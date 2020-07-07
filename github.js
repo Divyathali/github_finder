b4ef9b97b6576dc5e59c7cdb31077547abd2f4ee
Divyath ali shajahan <38224894+Divyathali@users.noreply.github.com>
2020-07-07 17:44:22 +0530

    Add files via upload

---
+++ github.js
@@ -0,0 +1,22 @@
+class Github {
+  constructor() {
+    this.client_id = '7c7ed8d53f12eecd34f2';
+    this.client_secret = '56a3683f45bcb5cadbb497450b868313c7ff2c4a';
+    this.repos_count = 5;
+    this.repos_sort = 'created: asc';
+  }
+
+  async getUser(user) {
+    const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+    const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+    const profile = await profileResponse.json();
+    const repos = await repoResponse.json();
+
+    return {
+      profile,
+      repos
+    };
+  }
+}
